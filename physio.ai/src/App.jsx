592e749a846c274d87a409f2d9a6b8123b7a0609
Subodh Badal <subodhbadal111@gmail.com>
2025-01-12 23:43:09 +0545

    added app.pu

--- physio.ai/src/App.jsx
+++ physio.ai/src/App.jsx
@@ -9,11 +9,15 @@ import Exercise from './components/ExcerciseCard'; // Direct import
 import Login from './components/login';
 import About from './components/about.jsx';
 import Chat from './components/Chat.jsx';
+import { GoogleOAuthProvider } from '@react-oauth/google';
+
 
 const App = () => {
   const [exercises, setExercises] = useState('');
 
   return (
+    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
+
     <Router>
       <Navbar/>
       <Routes>
@@ -21,10 +25,11 @@ const App = () => {
         <Route path="/about" element={<About />} />
         <Route path="/exercise" element={<Exercise />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Register />} />
         <Route path="/chat" element={<Chat setExercises={setExercises} />} />
       </Routes>
     </Router>
-    
+    </GoogleOAuthProvider>
      
   )
 }
