e77adf6b3e357fa2423fec24f4108a24b3f1bedf
Shrawan Gorathoki <shrawan44425@gmail.com>
2025-01-12 23:56:13 +0545

    added getAllUsers

--- node/src/controllers/userController.js
+++ node/src/controllers/userController.js
@@ -17,3 +17,21 @@ export const getProfile = async (req, res) => {
         message: 'User not found',
       });
     }
+    // Flatten the response structure to directly return the user data
+    res.status(200).json({
+        status: 'success',
+        user: {
+          email: user.email,
+          fullName: user.fullName,
+          role: user.role,
+          _id: user._id,
+          __v: user.__v
+        },
+      });
+    } catch (err) {
+      res.status(500).json({
+        status: 'error',
+        message: err.message,
+      });
+    }
+  };
