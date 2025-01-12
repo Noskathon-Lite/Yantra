903fab499088912f2399e7933f575b77f4d8ebe7
Shrawan Gorathoki <shrawan44425@gmail.com>
2025-01-12 23:57:58 +0545

    added getAllUsers

--- node/src/controllers/userController.js
+++ node/src/controllers/userController.js
@@ -35,3 +35,22 @@ export const getProfile = async (req, res) => {
       });
     }
   };
+
+  // Get all users
+export const getAllUsers = async (req, res) => {
+    try {
+      const users = await User.find();
+      res.status(200).json({
+        status: 'success',
+        results: users.length,
+        data: {
+          users
+        }
+      });
+    } catch (err) {
+      res.status(500).json({
+        status: 'error',
+        message: err.message
+      });
+    }
+  };
